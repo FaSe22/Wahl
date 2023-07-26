aab1acf01bcd75f803c16270b604d2a014516ee0
Sebastian <fase22290@icloud.com>
2023-07-26 21:26:04 +0200

    remove pest from dev-dependencies

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,18 +31,6 @@ class ComposerAutoloaderInit430a61f88067eac681e6127f35350284
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit430a61f88067eac681e6127f35350284::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
